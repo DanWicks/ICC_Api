dc3ccb1b51eb2a033eeb723d7e16df4a589f230b
Daniel Wicks <dan.wicks@outlook.com>
2016-03-13 16:49:05 -0400

    Origin Fixes v2
    Attempts to fix POST from localost

--- testApi/server.js
+++ testApi/server.js
@@ -29,6 +29,8 @@ var router = express.Router();// get an instance of the express Router
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // do logging
     console.log("Request received...");
     next(); // make sure we go to the next routes and don"t stop here
