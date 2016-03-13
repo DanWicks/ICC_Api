eac78edaa5325f2dc977ecf726f53821c618c22f
Daniel Wicks <dan.wicks@outlook.com>
2016-03-13 15:38:55 -0400

    Error messages
    made error messages more descriptive of whats going on inside

--- testApi/server.js
+++ testApi/server.js
@@ -25,7 +25,7 @@ var router = express.Router();// get an instance of the express Router
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log("Something is happening.");
+    console.log("Request received...");
     next(); // make sure we go to the next routes and don"t stop here
 });
 
@@ -63,32 +63,38 @@ router.route("/staff_info_by_id").post(function(req,res) {
 	});
 });
 // more routes for our API will happen here
-router.route("/staff_login").post(function(req,res) {	
+router.route("/staff_login").post(function(req,res) {		
+	console.log("Login endpoint request...");
 	pg.connect(connectionString, function(err, client, done) {
 		if(err) {
 			return console.error("Error: error fetching client from pool: ", err);
 		}	
 		var data = {"staff_id":req.body.staff_id, "password":req.body.password};
-		var results = [];	
+		var results = [];
 		if(data.staff_id == null || data.staff_id == "") {
 			return res.json({ "valid":false, "error":"No staff_id was found." });			
 		} else if (data.password == null || data.password == "") {
 		    return res.json({ "valid":false, "error":"No password was found." });
 		} else {
+			console.log("Login Attempt: " + "ID: " + data.staff_id + " PASS: " + data.password);
 			var sqlString = "SELECT * " +
-                  	"FROM Staff;" + 
-                  	"WHERE staff_id="+data.staff_id+" AND password='"+data.password+"';" ;
+                  	"FROM Staff " + 
+                  	"WHERE staff_id='"+data.staff_id+"' AND staff_password='"+data.password+"';";
 			var queryResults = client.query(sqlString);
 
 			queryResults.on("error", function(error) {
-				return res.json({ "valid":false,"error":error});
+				return res.json({ "valid":false,"error":error.message});
 			});
 			queryResults.on("row", function(row) {
 				results.push(row);
 			});
 			queryResults.on("end", function() {
 				done();
+			if(results.length == 0) {
+				return res.json({ "valid":false,"error":"Invalid login information." });				
+			} else {				
 				return res.json({ "valid":true,"results":results });			
+			}
 			});
 		}		
 	});
