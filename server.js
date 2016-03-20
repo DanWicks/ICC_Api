20004bf65cad33da051144b478c90d9c4083cc1e
DanWicks <dan.wicks@outlook.com>
2016-03-19 20:20:13 -0400

    Fixed typo in path

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express          = require("express");        // call express
 var app              = express();                 // define our app using express
 var bodyParser       = require("body-parser");
 var pg 		         = require("pg");
-var connectionString = proccess.env.DATABASE_URL || "postgres://uwityljpwrsqju:OBnZlBE5jqpRFLUllAOfgpb8OA@ec2-54-83-22-48.compute-1.amazonaws.com:5432/dcji1sldavs0ts";
+var connectionString = process.env.DATABASE_URL || "postgres://uwityljpwrsqju:OBnZlBE5jqpRFLUllAOfgpb8OA@ec2-54-83-22-48.compute-1.amazonaws.com:5432/dcji1sldavs0ts";
 //
 //var validExpression  = /(\%27)|(\")|(\-\-)|(\%23)|(#)/ix;
 
